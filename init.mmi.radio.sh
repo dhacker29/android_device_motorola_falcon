4af9f5b059eb36e293114b1b41e71fc272fb522e
dhacker29 <dhackerdvm@gmail.com>
2014-01-24 22:16:38 -0600

    Need to make sure directory is there for symlink creation

--- init.mmi.radio.sh
+++ init.mmi.radio.sh
@@ -4,6 +4,10 @@
 # Update the multisim config based on Radio version
 radio=`getprop ro.boot.radio`
 case $radio in
+	"0x3") # Retail US
+		setprop persist.radio.multisim.config ""
+		setprop ro.mot.build.customerid verizon
+		;;
 	"0x5") # Dual SIM
 		# Temporarily remove setting of this, force users
 		# to manually set until device fully supports
@@ -11,8 +15,8 @@ case $radio in
 		setprop ro.mot.build.customerid RETBR
 		;;
 	"0x6") # Retail US
-	    setprop persist.radio.multisim.config ""
-	    setprop ro.mot.build.customerid retusa_aws
+		setprop persist.radio.multisim.config ""
+		setprop ro.mot.build.customerid retusa_aws
 		;;
 	*) # All others
 		setprop persist.radio.multisim.config ""
